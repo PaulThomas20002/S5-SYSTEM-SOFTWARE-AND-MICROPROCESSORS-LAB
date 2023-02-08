bc3f5ecf3cb1b525c9bb5f5921590976c59acd3c
Paul Thomas <66468931+PaulThomas20002@users.noreply.github.com>
2023-02-08 22:03:58 +0530

    Update palindrome.asm

--- Cycle-3/palindrome.asm
+++ Cycle-3/palindrome.asm
@@ -1,32 +1,34 @@
 ASSUME CS:CODE,DS:DATA
 DATA SEGMENT
-	MYSTRING DB 'MALAYALAM$'
-	STRING1 DB 'PALINDROME$'
-	STRING2 DB 'NOT PALINDROME$'
+        mystring db 'Paul thomas$'
+        string1 db 'palindrome$'
+        string2 db 'not palindrome$'
 DATA ENDS
 CODE SEGMENT
-	START: MOV AX,DATA
-		MOV DS,AX
-		LEA DI,MYSTRING
-		MOV SI,DI
-	BACK: MOV AL,[DI]
-		CMP AL,'$'
-		JZ BACK1
-		INC DI
-		JMP BACK
-	BACK1: DEC DI
-		CMP SI,DI
-		MOV AL, [SI]
-		CMP AL,[DI]
-		JNZ PRINT2
-		INC SI
-		JMP BACK1
-	PRINT1: LEA DX, STRING1
-		JMP L2
-	PRINT2: LEA DX,STRING2
-	    L2: MOV AH,09H
-		INT 21H
-		MOV AH,4CH
-		INT 21H
-CODE ENDS
-END START
+START:
+        MOV AX,DATA
+        MOV DS,AX
+        LEA DI,mystring
+        MOV SI,DI
+    BACK:MOV AL,[DI]
+        CMP AL,'$'
+        JZ SCAN
+        INC DI
+        JMP BACK
+ SCAN:  DEC DI
+         CMP SI,DI
+        JNC OUT1
+        MOV AL,[SI]
+        CMP AL,[DI]
+        JNZ OUT2
+        INC SI
+        JMP SCAN
+   OUT1:LEA DX,string1
+        JMP L2
+   OUT2:LEA DX, string2
+   L2: MOV AH,09H
+        INT 21H
+        MOV AH,4CH
+        INT 21H
+ CODE ENDS
+ END START
